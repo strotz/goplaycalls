347ab88ff7cc9668aa2a5ba70655ab7b6bf0caea
Alexei Strots <alexei.strots@bluerivertech.com>
2024-07-08 14:23:21 -0700

    parsing

--- gpc/recipe_test.go
+++ gpc/recipe_test.go
@@ -8,14 +8,38 @@ import (
 )
 
 func TestMakeRecipe(t *testing.T) {
-	r := strings.NewReader(`### call example.com
+	t.Run("simple GET", func(t *testing.T) {
+		r := strings.NewReader(`### call example.com
 GET example.com`)
-	steps, err := makeRecipe(r)
-	assert.NoError(t, err)
-	require.Len(t, steps, 1)
-	assert.Equal(t, step{
-		name:   "call example.com",
-		method: "GET",
-		url:    "example.com",
-	}, steps[0])
+		steps, err := makeRecipe(r)
+		assert.NoError(t, err)
+		require.Len(t, steps, 1)
+		assert.Equal(t, step{
+			name:   "call example.com",
+			method: "GET",
+			url:    "example.com",
+		}, steps[0])
+	})
+
+	t.Run("GET with request handler", func(t *testing.T) {
+		r := strings.NewReader(`### call example.com
+GET example.com
+
+> {%
+console.log("Hello")
+%}`)
+		steps, err := makeRecipe(r)
+		assert.NoError(t, err)
+		require.Len(t, steps, 1)
+		assert.Equal(t, step{
+			name:   "call example.com",
+			method: "GET",
+			url:    "example.com",
+			responseHandler: &script{
+				content: `
+console.log("Hello")
+`,
+			},
+		}, steps[0])
+	})
 }
